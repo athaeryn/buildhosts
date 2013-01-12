370011b58adc5d97207eee8c162985929bd60f51
Mike Anderson <athaeryn@me.com>
2013-01-12 03:13:09 -0600

    Made things a little DRYer.

--- buildhosts.rb
+++ buildhosts.rb
@@ -2,6 +2,9 @@ def readFile(filename)
     File.open(filename, 'r').read
 end
 
+ipv6 = '::1        '
+home = '127.0.0.1  '
+
 config = readFile("config").split("&")
 hosts = config[0].split(" ")
 ips = config[1].split(" ")
@@ -10,11 +13,11 @@ out = File.open('newhosts', 'w+')
 
 out.write readFile "header"
 (0..hosts.length - 1).each do |i|
-    out.write "::1        #{hosts[i]}\n"
-    out.write "127.0.0.1  #{hosts[i]}\n"
+    out.write "#{ipv6}#{hosts[i]}\n"
+    out.write "#{home}#{hosts[i]}\n"
     (0..ips.length - 1).each do |j|
-        out.write "::1        #{hosts[i]}.#{ips[j]}.xip.io\n"
-        out.write "127.0.0.1  #{hosts[i]}.#{ips[j]}.xip.io\n"
+        out.write "#{ipv6}#{hosts[i]}.#{ips[j]}.xip.io\n"
+        out.write "#{home}#{hosts[i]}.#{ips[j]}.xip.io\n"
     end
 end
 out.close
