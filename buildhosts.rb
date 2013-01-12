27f831cf0f1d1fc67ac6bc0fb777617bd4183d56
Mike Anderson <athaeryn@me.com>
2013-01-12 04:48:23 -0600

    Made the generator handle blank lines and hash comments.

--- buildhosts.rb
+++ buildhosts.rb
@@ -2,22 +2,25 @@ def readFile(filename)
     File.open(filename, 'r').read
 end
 
+def sanitize(string)
+    string.split("\n").sort.delete_if {|x| x.start_with?('#') || x.strip == ''}
+end
+
 ipv6 = '::1        '
 home = '127.0.0.1  '
 
-config = readFile("config").split("&")
-hosts = config[0].split(" ")
-ips = config[1].split(" ")
+config = readFile("config").split("&").each {|x| x.strip!}
+hosts = sanitize config[0]
+ips   = sanitize config[1]
 
 out = File.open('newhosts', 'w+')
-
 out.write readFile "header"
-(0..hosts.length - 1).each do |i|
-    out.write "#{ipv6}#{hosts[i]}\n"
-    out.write "#{home}#{hosts[i]}\n"
-    (0..ips.length - 1).each do |j|
-        out.write "#{ipv6}#{hosts[i]}.#{ips[j]}.xip.io\n"
-        out.write "#{home}#{hosts[i]}.#{ips[j]}.xip.io\n"
+hosts.each do |host|
+    out.write "#{ipv6}#{host}\n"
+    out.write "#{home}#{host}\n"
+    ips.each do |ip|
+        out.write "#{ipv6}#{host}.#{ip}.xip.io\n"
+        out.write "#{home}#{host}.#{ip}.xip.io\n"
     end
 end
 out.close
