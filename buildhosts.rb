7eba5001d97b2a15f75ebadeed99fdf0ee02181f
Mike Anderson <athaeryn@me.com>
2013-01-12 02:59:37 -0600

    Streamlined the readFile method.

--- buildhosts.rb
+++ buildhosts.rb
@@ -6,6 +6,8 @@ config = readFile("config").split("&")
 hosts = config[0].split(" ")
 ips = config[1].split(" ")
 
+out = File.open('newhosts', 'w+')
+
 puts readFile "header"
 (0..hosts.length - 1).each do |i|
     puts "::1        #{hosts[i]}"
@@ -15,3 +17,5 @@ puts readFile "header"
         puts "127.0.0.1  #{hosts[i]}.#{ips[j]}"
     end
 end
+
+out.close
