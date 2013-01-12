086a29cdb7f127af46d6424dc98a8e302b4e226d
Mike Anderson <athaeryn@me.com>
2013-01-12 02:43:45 -0600

    Hosts entries generating.

--- buildhosts.rb
+++ buildhosts.rb
@@ -8,4 +8,14 @@ def readFile(filename)
     return contents
 end
 
-puts readFile("header") + readFile("footer")
+hosts = readFile("hosts").split(" ")
+ips = readFile("ips").split(" ")
+
+(0..hosts.length - 1).each do |i|
+    puts "::1        #{hosts[i]}"
+    puts "127.0.0.1  #{hosts[i]}"
+    (0..ips.length - 1).each do |j|
+        puts "::1        #{hosts[i]}.#{ips[j]}"
+        puts "127.0.0.1  #{hosts[i]}.#{ips[j]}"
+    end
+end
