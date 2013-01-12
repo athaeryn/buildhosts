1bfa7fd517f2fa648800f262fabaf9489ca7935e
Mike Anderson <athaeryn@me.com>
2013-01-12 03:44:08 -0600

    Moved the generating outside the if '-e' else block so that it always gets done.

--- buildhosts.sh
+++ buildhosts.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [ "$1" == "-e" ]; then
     vim /Users/mike/git/buildhosts/config
-else
-    cd /Users/mike/git/buildhosts
-    ruby buildhosts.rb
-    sudo cp newhosts /etc/hosts
 fi
+cd /Users/mike/git/buildhosts
+ruby buildhosts.rb
+sudo cp newhosts /etc/hosts
