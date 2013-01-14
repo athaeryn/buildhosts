a246fcc097a9d6300ee281c9bc27cc4c59ba1edc
Mike Anderson <athaeryn@me.com>
2013-01-14 17:50:22 -0600

    Added a -l option to list hosts

--- buildhosts.sh
+++ buildhosts.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ "$1" == "-e" ]; then
     vim /Users/mike/git/buildhosts/config
+elif [ "$1" == "-l" ]; then
+    grep 127.0.0.1 /etc/hosts | grep -v xip.io | awk '{print $2}'
 fi
 cd /Users/mike/git/buildhosts
 ruby buildhosts.rb
 sudo cp newhosts /etc/hosts
+
