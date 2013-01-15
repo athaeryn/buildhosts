803e26c5d70f9efb4e8fb6d7d70f415153a29426
Mike Anderson <athaeryn@me.com>
2013-01-14 20:35:24 -0600

    Made the script exit after listing hosts when -l flag is passed.

--- buildhosts.sh
+++ buildhosts.sh
@@ -3,6 +3,7 @@ if [ "$1" == "-e" ]; then
     vim /Users/mike/git/buildhosts/config
 elif [ "$1" == "-l" ]; then
     grep 127.0.0.1 /etc/hosts | grep -v xip.io | awk '{print $2}'
+    exit 0
 fi
 cd /Users/mike/git/buildhosts
 ruby buildhosts.rb
