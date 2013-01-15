239edb3cc605ea3d2d60656aac2a1a00eec0a0ac
Mike Anderson <athaeryn@me.com>
2013-01-15 00:27:48 -0600

    Cleaned up the shell script a bit.

--- buildhosts.sh
+++ buildhosts.sh
@@ -5,7 +5,7 @@ elif [ "$1" == "-l" ]; then
     grep 127.0.0.1 /etc/hosts | grep -v xip.io | grep -v localhost | awk '{print $2}'
     exit 0
 fi
-ruby /Users/mike/git/buildhosts/buildhosts.rb
+cd /Users/mike/git/buildhosts
 ruby buildhosts.rb
-sudo cp /Users/mike/git/buildhosts/newhosts /etc/hosts
+sudo cp newhosts /etc/hosts
 
