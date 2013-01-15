0e24dfcf26942e6bce068093621b5284a0064d88
Mike Anderson <athaeryn@me.com>
2013-01-14 20:38:18 -0600

    Filter 'localhost' out of list.

--- buildhosts.sh
+++ buildhosts.sh
@@ -2,10 +2,10 @@
 if [ "$1" == "-e" ]; then
     vim /Users/mike/git/buildhosts/config
 elif [ "$1" == "-l" ]; then
-    grep 127.0.0.1 /etc/hosts | grep -v xip.io | awk '{print $2}'
+    grep 127.0.0.1 /etc/hosts | grep -v xip.io | grep -v localhost | awk '{print $2}'
     exit 0
 fi
-cd /Users/mike/git/buildhosts
+ruby /Users/mike/git/buildhosts/buildhosts.rb
 ruby buildhosts.rb
-sudo cp newhosts /etc/hosts
+sudo cp /Users/mike/git/buildhosts/newhosts /etc/hosts
 
