f121c7bdbce3659dc1003074e514a3faff009512
Mike Anderson <athaeryn@me.com>
2013-01-16 21:47:21 -0600

    Combined two of the chained greps.

--- buildhosts.sh
+++ buildhosts.sh
@@ -3,7 +3,7 @@ cd /Users/mike/git/buildhosts
 if [ "$1" == "-e" ]; then
     vim config
 elif [ "$1" == "-l" ]; then
-    grep 127.0.0.1 /etc/hosts | grep -v xip.io | grep -v localhost | awk '{print $2}'
+    grep 127.0.0.1 /etc/hosts | grep -Ev 'xip.io|localhost' | awk '{print $2}'
     exit 0
 fi
 ruby buildhosts.rb
