8a156c08db88deba2bd88abc22fa79d73880dfaa
Mike Anderson <athaeryn@me.com>
2013-01-16 23:45:40 -0600

    Added the option to edit the custom file with `hosts -c`. (I'm sure there's a better way to do this...)

--- buildhosts.sh
+++ buildhosts.sh
@@ -6,6 +6,12 @@ fi
 cd $HOSTSPATH 
 if [ "$1" == "-e" ]; then
     vim config
+elif [ "$1" == "-c" ]; then
+    vim custom
+elif [ "$1" == "-ec" ]; then
+    vim config ; vim custom
+elif [ "$1" == "-ce" ]; then
+    vim custom ; vim config 
 elif [ "$1" == "-l" ]; then
     grep 127.0.0.1 /etc/hosts | grep -Ev 'xip.io|localhost' | awk '{print $2}'
     exit 0
