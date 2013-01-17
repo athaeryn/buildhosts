b2e84e1b9dd8039955b6467b80e33bac520c7462
Mike Anderson <athaeryn@me.com>
2013-01-16 21:33:33 -0600

    Moved the cd to the top.

--- buildhosts.sh
+++ buildhosts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+cd /Users/mike/git/buildhosts
 if [ "$1" == "-e" ]; then
-    vim /Users/mike/git/buildhosts/config
+    vim config
 elif [ "$1" == "-l" ]; then
     grep 127.0.0.1 /etc/hosts | grep -v xip.io | grep -v localhost | awk '{print $2}'
     exit 0
 fi
-cd /Users/mike/git/buildhosts
 ruby buildhosts.rb
 sudo cp newhosts /etc/hosts
 
