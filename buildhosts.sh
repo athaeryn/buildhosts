9a7bbe72c16737cc971b1413c087db0678e3fe1a
Mike Anderson <athaeryn@me.com>
2013-03-12 17:10:03 -0500

    Change -nginx flag to --nginx.

--- buildhosts.sh
+++ buildhosts.sh
@@ -15,7 +15,7 @@ elif [ "$1" == "-ce" ]; then
 elif [ "$1" == "-l" ]; then
     grep 127.0.0.1 /etc/hosts | grep -Ev 'xip.io|localhost' | awk '{print $2}'
     exit 0
-elif [ "$1" == "-nginx" ]; then
+elif [ "$1" == "--nginx" ]; then
     ruby manginx.rb
     exit 0
 fi
