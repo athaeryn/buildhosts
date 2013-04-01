9c3d3e35c63c572069733ddf818bfcbdd3662f84
Mike Anderson <athaeryn@me.com>
2013-04-01 17:34:57 -0500

    Made it actually work.

--- lib/buildhosts.rb
+++ lib/buildhosts.rb
@@ -3,17 +3,17 @@ require "buildhosts/ConfigParser"
 
 module Buildhosts
     class Main
-        def self.build
+        def self.build config, newhosts
             # Prefixy strings
             ipv6 = '::1             '
             home = '127.0.0.1       '
 
             # Grab that parser and parse
             
-            conf = Buildhosts::ConfigParser.parse 'config'
+            conf = Buildhosts::ConfigParser.parse config
 
             # Write the lines to a "newhosts" file
-            out = File.open('newhosts', 'w+')
+            out = File.open(newhosts, 'w+')
             conf['hosts'].each do |host|
                 out.write "#{ipv6}#{host}\n"
                 out.write "#{home}#{host}\n"
